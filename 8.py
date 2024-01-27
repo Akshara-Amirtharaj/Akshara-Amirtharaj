b661787bd53f10e32bd799ecb9965718c1893f73
Akshara-Amirtharaj <157888145+Akshara-Amirtharaj@users.noreply.github.com>
2024-01-27 17:07:57 +0530

    Add files via upload

---
+++ 8.py
@@ -0,0 +1,13 @@
+N = int(input("Enter a number: "))
+
+for num in range(2, N + 1):
+    prime = True  
+
+    for i in range(2, num):
+        if num % i == 0:
+            prime = False
+            break
+
+    if prime:
+        print(num, end=' ')
+
